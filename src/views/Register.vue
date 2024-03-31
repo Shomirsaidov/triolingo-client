9735e7d48a0daba193a3274b9bd4253f9b61ee69
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2024-03-31 16:17:51 +0500

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -63,6 +63,7 @@ export default {
         }) 
         .catch(e => {
           this.error = "Пользователь уже существует !"
+          this.error = e
         })
         .finally(() => this.loading = false)
       } else {
